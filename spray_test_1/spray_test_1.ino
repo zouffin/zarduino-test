3f5aa07e63bdcac5cd61a901e5cf151bc60655cc
Stéphane BONIFFACY <stephane@boniffacy.fr>
2017-03-24 19:01:14 +0100

    add LED exemple

--- spray_test_1/spray_test_1.ino
+++ spray_test_1/spray_test_1.ino
@@ -1,125 +1,111 @@
 
-//#define LCD_ON 1
-
-#ifdef LCD_ON
-#include <Wire.h>
-#include <LCD.h>
-#include <LiquidCrystal_I2C.h>
-#endif
-
 #include <NewPing.h>
 #include <IRremote.h>
 
-/************ 
- *    LCD
- ************/
-#ifdef LCD_ON
-#define LCD_I2C_ADDR	0x3F  // Define I2C Address where the PCF8574A is
-#define LCD_BACKLIGHT_PIN 	3
-#define LCD_EN_PIN  2
-#define LCD_RW_PIN  1
-#define LCD_RS_PIN  0
-#define LCD_D4_PIN  4
-#define LCD_D5_PIN  5
-#define LCD_D6_PIN  6
-#define LCD_D7_PIN  7
+//#define DEBUG
 
-LiquidCrystal_I2C     lcd(LCD_I2C_ADDR,LCD_EN_PIN,LCD_RW_PIN,LCD_RS_PIN,LCD_D4_PIN,LCD_D5_PIN,LCD_D6_PIN,LCD_D7_PIN);
-#endif
+#define MAX_DISTANCE   200   // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-//Distance sensor
+#define PING_SPEED    50    // Ping minimum frequences 
+
+#define SPRAY_DELAY   500   // Spray duration
+#define SPRAY_MAX_DIST  140   // Spray max distance to trigger
+#define SPRAY_MIN_DIST  30    // Spray min distance to trigger
+
+/*********************
+ * DEFINE THE PINS   *
+ *********************/
+// pin to activate spray : HIGH = Trigged 
+#define SPRAY_PIN   2
+// pin to do a sound signal to determine the state of device ON(one bip)/OFF(two bip)
+#define BUZZ_PIN  10
+// IR received signal pin
+#define RECV_PIN  9
+// Distance sensor trigger and echo pins
 #define TRIGGER_PIN 12
 #define ECHO_PIN 8
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-
-#define PING_SPEED 50
-
-
-#define SPRAY_DELAY 500
-#define SPRAY_DISTANCE 140
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
+IRrecv irrecv(RECV_PIN); // IR setup receive pin
 
-//Flashing LED on Arduino board
-#define LED_PIN 13
-#define SPRAY_PIN 2
-#define BUZZ_PIN 10
-
-//IRremote
-#define RECV_PIN 9
-
-IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-unsigned long pingTimer, sprayTimer;     // Holds the next ping time.
-unsigned long distance;
-bool was_changed;
+unsigned long pingTimer;  // Holds the next ping time.
+unsigned long sprayTimer;   // Holds the spray duration, it should be no more than SPRAY_DURATION.     
+unsigned long distance;   // current distance.
 
-
-bool on,goSpray;
+bool on; // device is live or not
+bool was_changed; // determine if distance sensor was get
+bool goSpray; //to force spray
 
 
 void setup() {
+#ifdef DEBUG
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  pingTimer = sprayTimer = millis(); // Start now.
-  was_changed = false;
-#ifdef LCD_ON
-  lcd.begin (16,2);  //Size of LCD
-// Switch on the backlight
-  lcd.setBacklightPin(LCD_BACKLIGHT_PIN,POSITIVE);
-  lcd.setBacklight(HIGH);
-  lcd.home ();
 #endif
+  // at init we don't spray and device state is off
+  was_changed = false;
+  on = false;
+  // force the spray even is device state is off
+  goSpray = false;
+
+  //init pingTimer and spary timer
+  pingTimer = sprayTimer = millis(); // Start now.
+
+  //init spray and buzz pin to output
   pinMode(SPRAY_PIN, OUTPUT);  
-  pinMode (BUZZ_PIN, OUTPUT) ;
+  pinMode(BUZZ_PIN, OUTPUT) ;
   
   irrecv.enableIRIn(); // Initialise le recepteur
-  on = false;
-  goSpray = false;
 }
 
 void loop() {
   // Notice how there's no delays in this sketch to allow you to do other processing in-line while doing distance pings.
   unsigned long current = millis();
+    
   if (current >= pingTimer && on) {   // PING_SPEED milliseconds since last ping, do another ping.
     pingTimer += PING_SPEED;      // Set the next ping time.
     sonar.ping_timer(echoCheck); // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
   }
-#ifdef LCD_ON
- //lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Distance: ");
-  lcd.print(distance);
-  lcd.print("cm");
-   // Do other stuff here, really. Think of it as multi-tasking.
-#endif
 
-  if(current >= sprayTimer ) {
-    if((distance <= SPRAY_DISTANCE && distance > 30 && was_changed && on) || goSpray ) {
-    Serial.println("I'm going to spray !");
-    Serial.print("distance: ");Serial.print(distance);Serial.println(" cm");
+  if(current >= sprayTimer ) {  // SPRAY_DELAY milliseconds since last ping, check people standed behind.
+    if((distance <= SPRAY_MAX_DIST && distance > SPRAY_MIN_DIST && was_changed && on) || goSpray ) {
+#ifdef DEBUG
+      Serial.println("I'm going to spray !");
+      Serial.print("distance: ");Serial.print(distance);Serial.println(" cm");
+#endif 
+      // start the spray
       digitalWrite(SPRAY_PIN, HIGH);
+    // set the spray timer to stop
       sprayTimer = current + SPRAY_DELAY;
+      // set people standed behind to state not changed 
       was_changed = false;
+      // set gospray to no, cause the spray is spraying
       goSpray = false;
     }
     else {
+      // stop spray (time > spraytimer)
       digitalWrite(SPRAY_PIN, LOW);
     }
   }
-  
+
+  // REMOTE results decoded
   if (irrecv.decode(&results)) {
     switch(results.value) {
+      // received power pushed 
       case 0xffa25d:
+        // change device state on <=> off
         on = !on;
-        goSpray = false;
+        
         if(on) {
+        // one long beep to say i'm ON  
           digitalWrite(BUZZ_PIN, HIGH);
           delay(100);
           digitalWrite(BUZZ_PIN, LOW);
         }
         else {
+        // two short beeps to say i'm OFF  
           digitalWrite(BUZZ_PIN, HIGH);
           delay(75);
           digitalWrite(BUZZ_PIN, LOW);
@@ -128,11 +114,16 @@ void loop() {
           delay(75);
           digitalWrite(BUZZ_PIN, LOW);
         }
+#ifdef DEBUG
         Serial.println(on?"ON!!!":"OFF!!!");
+#endif
         break;
+      // received start pushed 
       case 0xff02fd:
         goSpray = true;
+#ifdef DEBUG
         Serial.println("GO Spray!!!");
+#endif
         break;
       default:
       Serial.println(results.value, HEX);
@@ -143,15 +134,13 @@ void loop() {
   
 }
 
-void echoCheck() { // Timer2 interrupt calls this function every 24uS where you can check the ping status.
+//I changed the file IRRemoteint.h to listen on timer 1
+
+void echoCheck() { // Timer2 (changed to timer1) interrupt calls this function every 24uS where you can check the ping status.
   // Don't do anything here!
   if (sonar.check_timer()) { // This is how you check to see if the ping was received.
     // Here's where you can add code.
-   // Serial.print("Ping: ");
     distance = sonar.ping_result / US_ROUNDTRIP_CM;
-    //Serial.print(distance); // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
-    //Serial.println("cm  ");
     was_changed = true;
-  
   }
 }
